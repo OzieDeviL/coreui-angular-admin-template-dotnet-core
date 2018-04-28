fe6b2844cd454cb2f4c7603ea5bdfbc7302189ef
David Laughlin <davidclaughlin@gmail.com>
2018-04-27 18:39:58 -0700

    Bare Layout Created

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // Import Containers
-import { DefaultLayoutComponent } from './containers';
+import { DefaultLayoutComponent, LayoutComponent } from './containers';
 
 import { P404Component } from './views/error/404.component';
 import { P500Component } from './views/error/500.component';
@@ -44,8 +44,49 @@ export const routes: Routes = [
     }
   },
   {
-    path: '',
+    path: 'template',
     component: DefaultLayoutComponent,
+    data: {
+      title: 'Home'
+    },
+    children: [
+      {
+        path: 'base',
+        loadChildren: './views/base/base.module#BaseModule'
+      },
+      {
+        path: 'buttons',
+        loadChildren: './views/buttons/buttons.module#ButtonsModule'
+      },
+      {
+        path: 'charts',
+        loadChildren: './views/chartjs/chartjs.module#ChartJSModule'
+      },
+      {
+        path: 'dashboard',
+        loadChildren: './views/dashboard/dashboard.module#DashboardModule'
+      },
+      {
+        path: 'icons',
+        loadChildren: './views/icons/icons.module#IconsModule'
+      },
+      {
+        path: 'notifications',
+        loadChildren: './views/notifications/notifications.module#NotificationsModule'
+      },
+      {
+        path: 'theme',
+        loadChildren: './views/theme/theme.module#ThemeModule'
+      },
+      {
+        path: 'widgets',
+        loadChildren: './views/widgets/widgets.module#WidgetsModule'
+      }
+    ]
+  },
+  {
+    path: '',
+    component: LayoutComponent,
     data: {
       title: 'Home'
     },
